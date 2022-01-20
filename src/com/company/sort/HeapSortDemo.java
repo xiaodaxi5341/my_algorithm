8accc37b992df0b220db3e30fbc8a582d0521d57
wb-hxh573609 <wb-hxh573609@antfin.com>
2022-01-20 19:01:09 +0800

    测试

--- src/com/company/sort/HeapSortDemo.java
+++ src/com/company/sort/HeapSortDemo.java
@@ -1,11 +1,21 @@
 package com.company.sort;
 
-//堆排序是建立在完全二叉树的基础上
+//堆排序是建立在完全二叉树的基础上  此处写大根堆
 public class HeapSortDemo {
+
+    //堆容量
+    private int size;
+    //
+    private int cur;
+
+
 
     //往对结构插入数据
     private static void heapInsert(int[] arr,int a){
-
+        //假设arr已经是一个大根堆,且未满
+        //思路：先将数据插入数组末尾，随后与其父节点进行比较交换，直到不能交换为止，此时的结构就是大根堆结构
     }
+
+
 
 }
--- src/com/company/sort/UpdateDemo.java
+++
@@ -1,4 +0,0 @@
-package com.company.sort;
-
-public class UpdateDemo {
-}

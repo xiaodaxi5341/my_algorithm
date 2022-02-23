5970c74377c54ca21f723e1a4fa27ab51349d35d
xiaodaxi5341 <18813152372@163.com>
2022-02-23 21:35:24 +0800

    小和问题

--- src/com/company/sort/SmallSum.java
+++ src/com/company/sort/SmallSum.java
@@ -68,57 +68,50 @@ public class SmallSum {
         return arr;
     }
 
-    public static int smallSum(int[] arr){
-        if (arr == null || arr.length<2){
+    public static int smallSum(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return 0;
         }
 
-        return process(arr,0,arr.length-1);
+        return process(arr, 0, arr.length - 1);
 
     }
 
-    public static int process(int[] arr,int left,int right){
+    public static int process(int[] arr, int left, int right) {
 
-        if (left == right){
+        if (left == right) {
             return 0;
         }
 
-        int mid = ((right-left)>>1)+left;
-        int result = 0;
-//        System.out.println(" ===== = == = start  =====");
-        result+=process(arr,left,mid);
-//        System.out.println(" ======= left end =====");
-        result+=process(arr,mid+1,right);
-//        System.out.println(" ======= right end === ");
-        result += merge(arr,left,mid,mid+1,right);
-
-//        System.out.println("===== merge end ======");
-        return result;
+        int mid = ((right - left) >> 1) + left;
+        return process(arr, left, mid)
+                + process(arr, mid + 1, right)
+                + merge(arr, left, mid, mid + 1, right);
     }
 
     private static int merge(int[] arr, int left, int mid, int midAdd1, int right) {
-        int[] help = new int[right-left+1];
+        int[] help = new int[right - left + 1];
         int i = 0;
         int leftIndex = left;
         int result = 0;
-        while (leftIndex<=mid && midAdd1<=right){
-            if (arr[leftIndex]>=arr[midAdd1]){
+        while (leftIndex <= mid && midAdd1 <= right) {
+            if (arr[leftIndex] >= arr[midAdd1]) {
                 help[i++] = arr[midAdd1++];
-            }else{
-                result += arr[leftIndex]*(right-midAdd1+1);
+            } else {
+                result += arr[leftIndex] * (right - midAdd1 + 1);
                 help[i++] = arr[leftIndex++];
             }
         }
 
-        while (leftIndex<=mid){
+        while (leftIndex <= mid) {
             help[i++] = arr[leftIndex++];
         }
 
-        while (midAdd1<=right){
+        while (midAdd1 <= right) {
             help[i++] = arr[midAdd1++];
         }
 
-        for (int j = left,m = 0; j<=right;j++,m++){
+        for (int j = left, m = 0; j <= right; j++, m++) {
             arr[j] = help[m];
         }
 
